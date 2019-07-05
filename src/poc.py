2512c5b7c0e4dbededebbaa9caa6756511e9baf8
Anders Spælling <spaelling@gmail.com>
2019-07-05 20:46:25 +0200

    added resolveRequest and minor changes

--- src/poc.py
+++ src/poc.py
@@ -9,10 +9,25 @@ from ariadne.asgi import GraphQL
 
 # NOTE: This is an easy way to get the bearer token https://blog.jongallant.com/2017/11/azure-rest-apis-postman/
 
+# add resource type to resources query
 type_def = """
     type Query {
         ResourceGroups(subscriptionId: String!): [ResourceGroup]
         VirtualMachines(subscriptionId: String!): [VirtualMachine]
+        Resources(subscriptionId: String!, resourceGroupName: String): [Resource]
+    }
+
+    type Resource {
+        id: String!
+        name: String!
+        type: String!        
+        location: String!
+        sku: Sku
+    }
+
+    type Sku {
+        name: String!
+        tier: String
     }
 
     type ResourceGroup {
@@ -26,6 +41,8 @@ type_def = """
         name: String!
         location: String!
         nic: NetworkInterface
+        size: String!
+        os: String!
     }
 
     type NetworkInterface {
@@ -34,6 +51,7 @@ type_def = """
         ip: String
         pipid: String
         publicIP: PublicIP
+        allocationMethod: String!
     }
 
     type PublicIP {
@@ -46,104 +64,84 @@ type_def = """
 query = ObjectType("Query")
 virtualMachine = ObjectType("VirtualMachine")
 networkInterface = ObjectType("NetworkInterface")
+resource = ObjectType("Resource")
 
-# @query.field("ResourceGroups")
-# def resolve_ResourceGroups(_, info, subscriptionId):  
-#     request = info.context["request"]
-#     apiVersion = '2017-05-10'
-#     uri = 'https://management.azure.com/subscriptions/%s/resourcegroups?api-version=%s' % (subscriptionId, apiVersion)
-#     Authorization = request.headers['Authorization']
-#     headers={"Authorization": Authorization}
-#     r = requests.get(uri, headers=headers)
-#     return r.json()['value']
+bindableSchemas = [query, virtualMachine, networkInterface, resource]
+
+async def resolveRequest(info, uri):
+    request = info.context["request"]
+    authorization = request.headers['Authorization']
+    headers = {"Authorization": authorization}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(uri, headers = headers) as resp:
+            data = await resp.json()
+    if data.get('error'):
+        print(data['error']['message']) # TODO: log error
+        return None
+    return data.get('value', data)
+
+@query.field("Resources")
+async def resolve_Resources(_, info, subscriptionId, resourceGroupName=None):  
+    apiVersion = '2018-05-01'
+    if resourceGroupName:
+        uri = 'https://management.azure.com/subscriptions/%s/resourcegroups/%s/resources?api-version=%s' % (subscriptionId, resourceGroupName, apiVersion)
+    else:
+        uri = 'https://management.azure.com/subscriptions/%s/resources?api-version=%s' % (subscriptionId, apiVersion)
+    return await resolveRequest(info, uri)
+
+@resource.field("sku")
+def resolve_sku(parent, info):
+    if 'sku' in parent:
+        return parent['sku']
 
 @query.field("ResourceGroups")
-async def resolve_ResourceGroups(_, info, subscriptionId):  
-    request = info.context["request"]
+async def resolve_ResourceGroups(_, info, subscriptionId):
     apiVersion = '2017-05-10'
     uri = 'https://management.azure.com/subscriptions/%s/resourcegroups?api-version=%s' % (subscriptionId, apiVersion)
-    Authorization = request.headers['Authorization']
-    headers={"Authorization": Authorization}
-    async with aiohttp.ClientSession() as session:
-        async with session.get(uri, headers=headers) as resp:
-            data = await resp.json()
-    return data['value']
-
-# @query.field("VirtualMachines")
-# def resolve_VirtualMachines(_, info, subscriptionId):
-#     request = info.context["request"]
-#     apiVersion = '2019-03-01'
-#     uri = 'https://management.azure.com/subscriptions/%s/providers/Microsoft.Compute/virtualMachines?api-version=%s' % (subscriptionId, apiVersion)
-#     Authorization = request.headers['Authorization']
-#     headers={"Authorization": Authorization}
-#     r = requests.get(uri, headers=headers)
-#     vms = []
-#     for vm in r.json()['value']: 
-#         vms.append({"id": vm['id'], "name": vm['name'], "location": vm['location'], "nicid": vm['properties']['networkProfile']['networkInterfaces'][0]['id']})
-#     return vms
+    return await resolveRequest(info, uri)
 
 @query.field("VirtualMachines")
 async def resolve_VirtualMachines(_, info, subscriptionId):
-    request = info.context["request"]
     apiVersion = '2019-03-01'
     uri = 'https://management.azure.com/subscriptions/%s/providers/Microsoft.Compute/virtualMachines?api-version=%s' % (subscriptionId, apiVersion)
-    Authorization = request.headers['Authorization']
-    headers={"Authorization": Authorization}
-    # r = requests.get(uri, headers=headers)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(uri, headers=headers) as resp:
-            data = await resp.json()
+    data = await resolveRequest(info, uri)
     vms = []
-    for vm in data['value']: 
-        vms.append({"id": vm['id'], "name": vm['name'], "location": vm['location'], "nicid": vm['properties']['networkProfile']['networkInterfaces'][0]['id']})
+    for vm in data: 
+        d = {
+            'id': vm['id'],
+            'name': vm['name'],
+            'size': vm['properties']['hardwareProfile']['vmSize'],
+            'location': vm['location'],
+            'nicid': vm['properties']['networkProfile']['networkInterfaces'][0]['id'],
+            'os': vm['properties']['storageProfile']['osDisk']['osType']
+            }
+        vms.append(d)
     return vms
 
 @virtualMachine.field("nic")
-async def resolve_followers(parent, info): 
-    request = info.context["request"] 
+async def resolve_networkInterface(parent, info): 
     apiVersion = '2019-02-01'
     resourceId = parent['nicid']
     uri = 'https://management.azure.com%s?api-version=%s' % (resourceId, apiVersion)
-    Authorization = request.headers['Authorization']
-    headers={"Authorization": Authorization}
-    # r = requests.get(uri, headers=headers)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(uri, headers=headers) as resp:
-            nic = await resp.json()    
-    # nic = r.json()
-    return {"id": nic['id'], "name": nic['name'], "ip": nic['properties']['ipConfigurations'][0]['properties']['privateIPAddress'], "pipid": nic['properties']['ipConfigurations'][0]['properties']['publicIPAddress']['id']}
+    nic = await resolveRequest(info, uri)
+    return {
+        "id": nic['id'], 
+        "name": nic['name'], 
+        "ip": nic['properties']['ipConfigurations'][0]['properties']['privateIPAddress'], 
+        "pipid": nic['properties']['ipConfigurations'][0]['properties']['publicIPAddress']['id'],
+        'allocationMethod' : nic['properties']['ipConfigurations'][0]['properties']['privateIPAllocationMethod']
+        }
 
 @networkInterface.field("publicIP")
-async def resolve_publicIP(parent, info): 
-    request = info.context["request"] 
+async def resolve_publicIP(parent, info):
     apiVersion = '2019-02-01'
     resourceId = parent['pipid']
     uri = 'https://management.azure.com%s?api-version=%s' % (resourceId, apiVersion)
-    # print(uri)
-    Authorization = request.headers['Authorization']
-    headers={"Authorization": Authorization}
-    # r = requests.get(uri, headers=headers)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(uri, headers=headers) as resp:
-            data = await resp.json()   
-    pip =  {"id": data['id'], "name": data['name']}
-    if 'ipAddress' in data['properties']:
-        pip['ip'] = data['properties']['ipAddress']
+    data = await resolveRequest(info, uri)
+    pip =  {'id': data['id'], 'name': data['name'], 'ip': data['properties'].get('ipAddress')}
     return pip
 
-# @virtualMachine.field("nic")
-# def resolve_followers(parent, info): 
-#     request = info.context["request"] 
-#     apiVersion = '2019-02-01'
-#     resourceId = parent['nicid']
-#     uri = 'https://management.azure.com%s?api-version=%s' % (resourceId, apiVersion)
-#     Authorization = request.headers['Authorization']
-#     headers={"Authorization": Authorization}
-#     r = requests.get(uri, headers=headers)
-#     nic = r.json()
-#     return {"id": nic['id'], "name": nic['name'], "ip": nic['properties']['ipConfigurations'][0]['properties']['privateIPAddress']}
-
-schema = make_executable_schema(type_def, [query, virtualMachine, networkInterface])
+schema = make_executable_schema(type_def, bindableSchemas)
 app = GraphQL(schema, debug=True)
 
 if __name__ == "__main__":
