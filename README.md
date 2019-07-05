135fdd0b424b06781393cc478b6e9183767beba8
Anders Spælling <spaelling@gmail.com>
2019-07-05 21:37:02 +0200

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,151 @@
-# AzureGraphQL
-GraphQL Wrapper for the Azure management API
+# GraphQL Wrapper for the Azure management API
 
-Example query
+_Current state is proof of concept._
+
+## Getting started
+
+Python must be installed and some extra modules (I believe the following should cover it)
+
+```
+pip install ariadne
+pip install aiohttp
+pip install requests
+pip install uvicorn
+```
+
+Then simply run it
+
+```
+python '.\path\to\poc.py'
+```
+
+and visit [127.0.0.1:8000/](http://127.0.0.1:8000/) in your favorite browser.
+
+In the bottom left part of the editor the Bearer token must be entered in the following form:
+```graphql
+{"Authorization":"Bearer thisisaverylongonethatswhatshesaid"}
+```
+
+This is an easy way to get the [bearer token in 2 minutes](https://blog.jongallant.com/2017/11/azure-rest-apis-postman/) and just general nice way to get Postman quickly setup to work with the Azure API.
+
+## Example
+
+This shows the speed at which all of the data is gathered. The equivalent of 12 REST API calls. Some of which could be done asynchronously, but GraphQL does that and handles any dependencies at the same time.
+
+![Example](/media/example.gif)
+
+## Example
+
+Query to get virtual machines and *lots and lots* of nested properties that would require numerous calls to the Azure REST API:
 
 ```graphql
-{ VirtualMachines(subscriptionId: "b9334351-cec8-405d-8358-51846fa2a3ab") {
+{ VirtualMachines(subscriptionId: "xxx-xxxx-xxx") {
   name
+  size
+  os
+  location
   nic {
     name
     ip
-    publicIP {
+    networkSecurityGroup {
       name
+      rules {
+        name
+        protocol
+        access
+      }
+    }
+    subnet {
+      name
+      addressPrefix
+    }
+    publicIP {      
       ip
     }
   }
 }}
 ````
 
-Header
+Result:
 
 ```graphql
-{"Authorization":"Bearer xxxxxxxxxxxxxxxx"}
+{
+  "data": {
+    "VirtualMachines": [
+      {
+        "name": "certbot-01",
+        "size": "Standard_B1ms",
+        "os": "Linux",
+        "location": "westeurope",
+        "nic": {
+          "name": "certbot-01810",
+          "ip": "172.18.0.4",
+          "networkSecurityGroup": {
+            "name": "certbot-01-nsg",
+            "rules": [
+              {
+                "name": "SSH",
+                "protocol": "TCP",
+                "access": "Allow"
+              },
+              {
+                "name": "AllowHttpInbound",
+                "protocol": "TCP",
+                "access": "Allow"
+              },
+              {
+                "name": "AllowHttpsInbound",
+                "protocol": "TCP",
+                "access": "Allow"
+              }
+            ]
+          },
+          "subnet": {
+            "name": "default",
+            "addressPrefix": "172.18.0.0/24"
+          },
+          "publicIP": {
+            "ip": "40.113.144.28"
+          }
+        }
+      },
+      {
+        "name": "vm-pihole",
+        "size": "Standard_B1s",
+        "os": "Linux",
+        "location": "westeurope",
+        "nic": {
+          "name": "vm-pihole342",
+          "ip": "10.0.0.4",
+          "networkSecurityGroup": null,
+          "subnet": {
+            "name": "default",
+            "addressPrefix": "10.0.0.0/28"
+          },
+          "publicIP": {
+            "ip": "52.233.137.218"
+          }
+        }
+      },
+      {
+        "name": "Win10-WD002",
+        "size": "Standard_DS3_v2",
+        "os": "Windows",
+        "location": "westeurope",
+        "nic": {
+          "name": "win10-wd002",
+          "ip": "10.0.0.7",
+          "networkSecurityGroup": null,
+          "subnet": {
+            "name": "DtlwdlabSubnet",
+            "addressPrefix": "10.0.0.0/20"
+          },
+          "publicIP": {
+            "ip": null
+          }
+        }
+      }
+    ]
+  }
+}
 ```
--- src/poc.py
+++ src/poc.py
@@ -1,20 +1,15 @@
 import uvicorn
 import requests 
-
 import asyncio
 import aiohttp
-
 from ariadne import QueryType, gql, make_executable_schema, ObjectType, MutationType
 from ariadne.asgi import GraphQL
 
-# NOTE: This is an easy way to get the bearer token https://blog.jongallant.com/2017/11/azure-rest-apis-postman/
-
-# add resource type to resources query
 type_def = """
     type Query {
         ResourceGroups(subscriptionId: String!): [ResourceGroup]
         VirtualMachines(subscriptionId: String!): [VirtualMachine]
-        Resources(subscriptionId: String!, resourceGroupName: String): [Resource]
+        Resources(subscriptionId: String!, resourceGroupName: String, resourceType: String): [Resource]
     }
 
     type Resource {
@@ -52,6 +47,27 @@ type_def = """
         pipid: String
         publicIP: PublicIP
         allocationMethod: String!
+        networkSecurityGroup: NetworkSecurityGroup
+        subnet: Subnet
+    }
+
+    type Subnet {
+        id: String!
+        name: String!
+        addressPrefix: String!
+    }
+
+    type NetworkSecurityGroup {
+        name: String!
+        id: String!
+        rules: [SecurityRules]
+    }
+
+    type SecurityRules {
+        name: String!
+        id: String!
+        protocol: String
+        access: String!
     }
 
     type PublicIP {
@@ -61,10 +77,10 @@ type_def = """
     }
 """
 
-query = ObjectType("Query")
-virtualMachine = ObjectType("VirtualMachine")
-networkInterface = ObjectType("NetworkInterface")
-resource = ObjectType("Resource")
+query = ObjectType('Query')
+virtualMachine = ObjectType('VirtualMachine')
+networkInterface = ObjectType('NetworkInterface')
+resource = ObjectType('Resource')
 
 bindableSchemas = [query, virtualMachine, networkInterface, resource]
 
@@ -81,10 +97,12 @@ async def resolveRequest(info, uri):
     return data.get('value', data)
 
 @query.field("Resources")
-async def resolve_Resources(_, info, subscriptionId, resourceGroupName=None):  
+async def resolve_Resources(_, info, subscriptionId, resourceGroupName=None, resourceType=None):  
     apiVersion = '2018-05-01'
     if resourceGroupName:
         uri = 'https://management.azure.com/subscriptions/%s/resourcegroups/%s/resources?api-version=%s' % (subscriptionId, resourceGroupName, apiVersion)
+    elif resourceType:
+        None # TODO
     else:
         uri = 'https://management.azure.com/subscriptions/%s/resources?api-version=%s' % (subscriptionId, apiVersion)
     return await resolveRequest(info, uri)
@@ -105,6 +123,8 @@ async def resolve_VirtualMachines(_, info, subscriptionId):
     apiVersion = '2019-03-01'
     uri = 'https://management.azure.com/subscriptions/%s/providers/Microsoft.Compute/virtualMachines?api-version=%s' % (subscriptionId, apiVersion)
     data = await resolveRequest(info, uri)
+    if data == None:
+        return None
     vms = []
     for vm in data: 
         d = {
@@ -112,7 +132,7 @@ async def resolve_VirtualMachines(_, info, subscriptionId):
             'name': vm['name'],
             'size': vm['properties']['hardwareProfile']['vmSize'],
             'location': vm['location'],
-            'nicid': vm['properties']['networkProfile']['networkInterfaces'][0]['id'],
+            '_nicid': vm['properties']['networkProfile']['networkInterfaces'][0]['id'],
             'os': vm['properties']['storageProfile']['osDisk']['osType']
             }
         vms.append(d)
@@ -121,25 +141,59 @@ async def resolve_VirtualMachines(_, info, subscriptionId):
 @virtualMachine.field("nic")
 async def resolve_networkInterface(parent, info): 
     apiVersion = '2019-02-01'
-    resourceId = parent['nicid']
+    resourceId = parent['_nicid']
     uri = 'https://management.azure.com%s?api-version=%s' % (resourceId, apiVersion)
     nic = await resolveRequest(info, uri)
     return {
         "id": nic['id'], 
         "name": nic['name'], 
-        "ip": nic['properties']['ipConfigurations'][0]['properties']['privateIPAddress'], 
-        "pipid": nic['properties']['ipConfigurations'][0]['properties']['publicIPAddress']['id'],
-        'allocationMethod' : nic['properties']['ipConfigurations'][0]['properties']['privateIPAllocationMethod']
+        "ip": nic['properties']['ipConfigurations'][0]['properties']['privateIPAddress'],         
+        'allocationMethod' : nic['properties']['ipConfigurations'][0]['properties']['privateIPAllocationMethod'],
+        "_pipid": nic['properties']['ipConfigurations'][0]['properties'].get('publicIPAddress',{}).get('id'),
+        "_subnetid": nic['properties']['ipConfigurations'][0]['properties'].get('subnet',{}).get('id'),
+        "_nsgid": nic['properties'].get('networkSecurityGroup', {}).get('id')
         }
 
 @networkInterface.field("publicIP")
 async def resolve_publicIP(parent, info):
     apiVersion = '2019-02-01'
-    resourceId = parent['pipid']
+    resourceId = parent['_pipid']
+    if resourceId == None:
+        return None    
     uri = 'https://management.azure.com%s?api-version=%s' % (resourceId, apiVersion)
     data = await resolveRequest(info, uri)
     pip =  {'id': data['id'], 'name': data['name'], 'ip': data['properties'].get('ipAddress')}
     return pip
+
+@networkInterface.field("networkSecurityGroup")
+async def resolve_nsg(parent, info):
+    apiVersion = '2019-02-01'
+    resourceId = parent.get('_nsgid')
+    if resourceId == None:
+        return None
+    uri = 'https://management.azure.com%s?api-version=%s' % (resourceId, apiVersion)
+    data = await resolveRequest(info, uri)
+    nsg =  {'id': data['id'], 'name': data['name'], 'rules': []}
+    for rule in data['properties']['securityRules']:
+        p = rule['properties']
+        nsg['rules'].append({
+            'name': rule['name'],
+            'id': rule['id'],
+            'protocol': p['protocol'],
+            'access': p['access']
+        })
+    return nsg
+
+@networkInterface.field("subnet")
+async def resolve_subnet(parent, info):
+    apiVersion = '2019-02-01'
+    resourceId = parent.get('_subnetid')
+    if resourceId == None:
+        return None
+    uri = 'https://management.azure.com%s?api-version=%s' % (resourceId, apiVersion)
+    data = await resolveRequest(info, uri)
+    subnet =  {'id': data['id'], 'name': data['name'], 'addressPrefix': data['properties']['addressPrefix']}
+    return subnet
 
 schema = make_executable_schema(type_def, bindableSchemas)
 app = GraphQL(schema, debug=True)
